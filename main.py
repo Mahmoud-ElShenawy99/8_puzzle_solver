ee6a1e294b8e642029a2d9c8b90634d1ad051ca4
MohamedEl-Sherif <90091556+MohamedEl-Sherif@users.noreply.github.com>
2022-10-31 21:10:58 +0200

    state_goall_sherif

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from InformedSearchAgents import DFS
 
 if __name__ == '__main__':
     initial = 312405678
-    goal = 12345678
-    DFS(initial, goal)
+    goal_state = 12345678
+    DFS(initial, goal_state)
     print("{DFS Done}")
